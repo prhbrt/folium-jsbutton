c0faf416b616e2507d8444ba16d6d8f3288546b6
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2018-09-28 10:08:24 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='folium-jsbutton',
     version=__import__('folium_jsbutton').__version__,
 
-    description='',
+    description=('Add a button to a folium map that executes javascript code, by specifying a function in a python '
+                 'string that gets the button and map elements as parameters.'),
     long_description='',
 
     url='https://github.com/prinsherbert/folium-jsbutton',
@@ -14,7 +15,6 @@ setup(
 
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Map people',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -26,6 +26,7 @@ setup(
 
     install_requires=[
         'folium>=0.6.0',
+        'branca>=0.3.0',
     ],
     zip_safe=True,
 )
